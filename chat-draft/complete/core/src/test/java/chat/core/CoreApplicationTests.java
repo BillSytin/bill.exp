b2e73ac272495ef274ea723dac9efd7c8ef92ba9
bsytin <bsytin@knoema.com>
2018-11-16 20:10:39 +0500

    test wait

--- chat-draft/complete/core/src/test/java/chat/core/CoreApplicationTests.java
+++ chat-draft/complete/core/src/test/java/chat/core/CoreApplicationTests.java
@@ -2,6 +2,8 @@ package chat.core;
 
 import chat.core.model.*;
 import chat.core.util.Stoppable;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.Semaphore;
 
 @SuppressWarnings({"unused", "EmptyMethod"})
 @RunWith(SpringRunner.class)
@@ -84,5 +88,78 @@ public class CoreApplicationTests {
 		Assert.assertEquals(inputMessage.getAction(), outputMessage.getAction());
 		Assert.assertEquals(inputMessage.getStatus(), outputMessage.getStatus());
 		Assert.assertEquals(inputMessage.getContent(), outputMessage.getContent());
+	}
+
+	private static final int LONG_WAIT = 10000;
+
+	@Test(timeout = LONG_WAIT / 2)
+	public void shouldNotHang() throws InterruptedException {
+
+		Manager manager = new Manager();
+		Thread waiter = new Thread(() -> manager.waitStopped(LONG_WAIT));
+		waiter.start();
+		manager.setStopped();
+		waiter.join();
+	}
+
+	static class Manager {
+
+		private final Object completeEvent;
+		private final Semaphore stopPointReachedOut = new Semaphore(0);
+		private final Semaphore waitPointReachedOut = new Semaphore(0);
+		private volatile boolean isStopping;
+		private volatile boolean isStopped;
+
+		Manager() {
+			completeEvent = new Object();
+			isStopping = false;
+			isStopped = false;
+		}
+
+		void setStopped() throws InterruptedException {
+
+			waitPointReachedOut.acquire(1);
+			if (!isStopped) {
+
+				synchronized (completeEvent) {
+				    if (!isStopped) {
+                        isStopped = true;
+                        completeEvent.notifyAll();
+                    }
+				}
+
+				stopPointReachedOut.release();
+			}
+		}
+
+		boolean waitStopped(int timeout) {
+
+			if (!isStopped) {
+
+				waitPointReachedOut.release();
+				try {
+					stopPointReachedOut.acquire(1);
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+					return false;
+				}
+
+				synchronized (completeEvent) {
+
+				    if (!isStopped) {
+                        try {
+                            if (timeout < 0) {
+                                completeEvent.wait();
+                            } else {
+                                completeEvent.wait(timeout);
+                            }
+                        } catch (final InterruptedException e) {
+                            return false;
+                        }
+                    }
+				}
+			}
+			return isStopped;
+		}
 	}
 }
